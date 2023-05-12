83832f4a8687cc360fd728ca8c29a0cbf8880324
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-05-12 22:23:38 +0300

    .

--- cocktail-recipe-app/layouts/default.vue
+++ cocktail-recipe-app/layouts/default.vue
@@ -16,20 +16,6 @@
 import CocktailHeroData from "../data/HeroCocktailsData.json";
 const data = CocktailHeroData;
 
-/* let Dete = ref([]);
-
-const funcData= async ()=>{
-    const { data: response } = await useFetch("https://api.api-ninjas.com/v1/cocktail?name=blood", {
-    
-    headers:{
-        "X-Api-Key": "ZaU0i24xLhaL8BuCUYF5Ww==GRKu390gRSYDSWyt"
-    }
-
-});
-Dete.value = toRaw(response.value);
-} 
-funcData();
-console.log(Dete); */
 
 
 const base_url = useRuntimeConfig();
