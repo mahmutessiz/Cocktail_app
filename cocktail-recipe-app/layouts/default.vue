b1e8ac3cca47a70ff2b0ac8e5e05f280bf41ddc8
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-05-12 22:20:42 +0300

    import api

--- cocktail-recipe-app/layouts/default.vue
+++ cocktail-recipe-app/layouts/default.vue
@@ -8,13 +8,47 @@
 {{ data }}
 
     </div>
-    
+
 </template>
 
 <script setup>
 
 import CocktailHeroData from "../data/HeroCocktailsData.json";
 const data = CocktailHeroData;
+
+/* let Dete = ref([]);
+
+const funcData= async ()=>{
+    const { data: response } = await useFetch("https://api.api-ninjas.com/v1/cocktail?name=blood", {
+    
+    headers:{
+        "X-Api-Key": "ZaU0i24xLhaL8BuCUYF5Ww==GRKu390gRSYDSWyt"
+    }
+
+});
+Dete.value = toRaw(response.value);
+} 
+funcData();
+console.log(Dete); */
+
+
+const base_url = useRuntimeConfig();
+
+
+let Dete = ref([]);
+
+const funcData= async ()=>{
+    const { data: response } = await useFetch(base_url.public.apiBase + "blood", {
+    
+    headers:{
+        "X-Api-Key": base_url.apiSecret
+    }
+
+});
+Dete.value = toRaw(response.value);
+} 
+funcData();
+console.log(Dete);
 
 </script>
 
--- cocktail-recipe-app/nuxt.config.ts
+++ cocktail-recipe-app/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    // Private config that is only available on the server
+    apiSecret: '123',
+    // Config within public will be also exposed to the client
+    public: {
+      apiBase: '/api'
+    }
+  },
     css: ['~/assets/css/tailwind.css'],
     postcss: {
         plugins: {
@@ -7,4 +15,6 @@ export default defineNuxtConfig({
           autoprefixer: {},
         },
       },
+
 })
+  
