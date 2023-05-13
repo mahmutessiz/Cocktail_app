5954e035f28e6ccd269c3a19b27dd61789bf2631
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-05-13 17:18:06 +0300

    add cards

--- cocktail-recipe-app/components/CocktailRecipeCard.vue
+++ cocktail-recipe-app/components/CocktailRecipeCard.vue
@@ -1,35 +1,15 @@
 <template>
-    <div class="w-full flex justify-around px-8 py-8">
-        <div class="w-96 overflow-hidden rounded-lg shadow-sm bg-rose-500 shadow-black/30">
-            <img src="../assets/cocktail_images/martini.png" class="object-cover object-center w-full aspect-video"
+    <div class="card-container">
+        <div class="card" v-for="(cocktail, index) in cocktailData.cocktails" :key="index">
+            <img :src="cocktail.image" class="object-cover object-center w-full aspect-video"
                 alt="martini image">
             <div>
                 <ul class="flex flex-col justify-between py-4 px-2">
-                    <li class="text-black font-bold text-xl">Martini</li>
-                    <li class="text-black/80 line-clamp-4">A martini cocktail is a mixed drink that usually consists of gin
-                        and vermouth, and is garnished with an olive or a lemon twist. It is one of the most famous and
-                        classic cocktails in the world. </li>
-                    <li class="w-full flex justify-end px-4 pt-4"><button
+                    <li class="text-black font-bold text-xl"> {{ cocktail.name }}</li>
+                    <li class="text-black/80 line-clamp-4"> {{ cocktail.description }} </li>
+                    <li class="w-full flex justify-end px-4 pt-4"><span
                             class="border border-black/20 bg-rose-900 text-white text-sm shadow-md shadow-black/30 hover:scale-105 p-1 rounded-md">View
-                            more</button></li>
-                </ul>
-            </div>
-        </div>
-
-        <!-- card 2 -->
-
-        <div class="w-96 overflow-hidden rounded-lg shadow-sm bg-rose-500 shadow-black/30">
-            <img src="../assets/cocktail_images/mojito.png" class="object-cover object-center w-full aspect-video"
-                alt="mojito image">
-            <div>
-                <ul class="flex flex-col justify-between py-4 px-2">
-                    <li class="text-black font-bold text-xl">Mojito</li>
-                    <li class="text-black/80 line-clamp-4">A martini cocktail is a mixed drink that usually consists of gin
-                        and vermouth, and is garnished with an olive or a lemon twist. It is one of the most famous and
-                        classic cocktails in the world. </li>
-                    <li class="w-full flex justify-end px-4 pt-4"><button
-                            class="border border-black/20 bg-rose-900 text-white text-sm shadow-md shadow-black/30 hover:scale-105 p-1 rounded-md">View
-                            more</button></li>
+                            more</span></li>
                 </ul>
             </div>
         </div>
@@ -37,6 +17,8 @@
 </template>
 
 <script setup>
+import CocktailData from "../data/HeroCocktailsData.json";
 
+const cocktailData = CocktailData;
 </script>
 
--- cocktail-recipe-app/components/MainMenu.vue
+++ cocktail-recipe-app/components/MainMenu.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <header class="w-full bg-rose-600 flex py-4 px-8 justify-between items-center">
-      <div>Cocktail</div>
-      <div class="flex">
+      <div class="font-bold">Cocktail</div>
+      <div class="sm:flex hidden">
         <input type="text" class="rounded-lg rounded-r-none px-2" placeholder="Search cocktail">
         <button type="submit" class="bg-fuchsia-950 py-2 px-1 rounded-md rounded-l-none text-white">search</button>
       </div>
