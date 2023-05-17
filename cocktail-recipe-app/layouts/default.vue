9752b4ae4ebee73f4a8ae8ce01c992b9037fa5e7
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-05-17 12:08:43 +0300

    seo

--- cocktail-recipe-app/layouts/default.vue
+++ cocktail-recipe-app/layouts/default.vue
@@ -10,3 +10,21 @@
 </template>
 
 
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  title: "Cocktail App",
+  meta: [
+    {
+      name: 'description',
+      content: 'This is a cocktail website. You can find and learn how to make cocktails.'
+    },
+    {
+      name: "keywords",
+      content: "cocktail"
+    }
+  ]
+})
+</script>
